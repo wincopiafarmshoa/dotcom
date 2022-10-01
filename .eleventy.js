cf2dc77f4e30127982f456c87a6c76b400aadffd
Dan Slaughter <danslaughter2@gmail.com>
2022-10-01 07:30:52 -0400

    Starts adding module types.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/fonts');
     eleventyConfig.addPassthroughCopy('./src/admin');
 
     return {
