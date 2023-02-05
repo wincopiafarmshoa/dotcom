30270d3e86b9c30c32663e0a04f5fa8bb3f7e2e9
Dan Slaughter <danslaughter2@gmail.com>
2023-02-05 07:48:07 -0500

    Site updates.

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -3,7 +3,15 @@
 window.addEventListener("load", function(event) {
 	let burger = document.querySelector('.wf-burger-menu')
 	let nav = document.querySelector('.wf-header-navigation-list')
-	burger.addEventListener('click', function () { 
+	let navItems = document.querySelectorAll('.wf-header-navigation-list-item')
+
+	burger.addEventListener('click', function () {
 		nav.classList.toggle('open')
-	})
+	});
+
+	for (let i = 0; i < navItems.length; i++) {
+		navItems[i].addEventListener('click', function () {
+			navItems[i].classList.toggle('open')
+		});
+	}
 });
